def52ddf0452af15ddead7288a7f20068801c251
yuhyeon <klyhyeon@gmail.com>
2021-10-04 15:43:57 +0900

    feat: 빗썸 코테 String 문제추가

---
+++ src/main/bitthumb/Dictionary.java
@@ -0,0 +1,11 @@
+package bitthumb;
+
+public class Dictionary {
+    /**
+     * 주어진 단어의 알파벳으로 단어를 조합해 현재 단어의 알파벳 순서 그 다음 단어를 return 해주는 문제
+     */
+    public String solution(String s) {
+        String answer = "";
+        return answer;
+    }
+}
